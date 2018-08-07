1854053bc1da1d45de1819313e20ab09b1e08896
Manjunath Maigur <manjumbmb97@gmail.com>
2018-08-08 00:38:45 +0530

    Changed structure of web page, made form to collect data and store it in database

--- plotterapp/forms.py
+++ plotterapp/forms.py
@@ -1,3 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, SubmitField
+from wtforms import StringField,FloatField, SelectField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
+
+class NewProjectForm(FlaskForm):
+	project_name = StringField('Project Name', validators=[DataRequired()])
+	SCALES = [
+		('in', 'inch'),
+		('ft', 'feet'),
+		('m', 'meter'),
+	]
+	scale_type = SelectField('Select scale', choices=SCALES, validators=[DataRequired()])
+	width = FloatField('Width', validators=[DataRequired()])
+	height = FloatField('Height', validators=[DataRequired()])
+	mounting_distance = FloatField('Distance between mounting points', validators=[DataRequired()])
+	origin_distance = FloatField('Vertical distance to origin')
+	submit = SubmitField('Create')
--- plotterapp/routes.py
+++ plotterapp/routes.py
@@ -1,7 +1,35 @@
-from flask import render_template, flash, redirect, url_for
-from plotterapp import plotterapp
+from flask import render_template, flash, redirect, url_for, session
+from flask_wtf import form
+from plotterapp import plotterapp, db
+from .models import Canvas
+from .forms import NewProjectForm
 
 @plotterapp.route('/')
 @plotterapp.route('/index')
 def index():
 	return render_template('index.html')
+
+@plotterapp.route('/help')
+def help():
+	return render_template('help.html')
+
+@plotterapp.route('/start-project', methods=['GET', 'POST'])
+def project_form():
+	form = NewProjectForm()
+	if form.validate_on_submit():
+		project = Canvas()
+		project.project_name = form.project_name.data
+		project.scale_type = form.scale_type.data
+		project.width = form.width.data
+		project.height = form.height.data
+		project.mounting_distance = form.mounting_distance.data
+		project.origin_distance = form.origin_distance.data
+		db.session.add(project)
+		db.session.commit()
+		return redirect(url_for('canvas',id=project.id))
+	return render_template('project_form.html', form=form)
+
+@plotterapp.route('/canvas/<id>')
+def canvas(id):
+	project = Canvas.query.filter_by(id=id).first_or_404()
+	return render_template('canvas.html',project=project)
