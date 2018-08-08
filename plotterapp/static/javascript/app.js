b5df4818e89df56d2dfdb667ed8ab0c68ffa8c25
Ashwin Shenoy <ashwinshenoy05@gmail.com>
2018-08-08 07:15:15 +0530

    Added some changes to app.js

--- plotterapp/static/javascript/app.js
+++ plotterapp/static/javascript/app.js
@@ -1,2 +1,9 @@
+// Set the canvas id to the variable canvas
 var canvas = document.getElementById('myCanvas');
+
+// Get the context 2d for the canvas
 var ctx = canvas.getContext('2d');
+
+// Define arrays for storing the pixel values corresponding to the drawing the user has made.
+var mouse_X_pos = new Array();
+var mouse_Y_pos = new Array();
