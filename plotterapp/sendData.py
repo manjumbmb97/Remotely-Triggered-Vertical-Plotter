a77096bafdefed6073fae305c3f68247add2c876
Manjunath Maigur <manjumbmb97@gmail.com>
2018-11-12 22:35:58 +0530

    update

--- plotterapp/sendData.py
+++ plotterapp/sendData.py
@@ -1,5 +1,5 @@
 import serial
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 import time
 
 
@@ -20,22 +20,23 @@ def sendCoordinates():
 	serData = "0,16435,17126"
 	ser.write(serData.encode())
 
-	rFlag = False
-	lFlag = False
-	
-	while True:
-		readData = ser.read()
-		if str(readData.decode()) == "r":
-			rFlag = True
-		if str(readData.decode()) == "l":
-			lFlag = True
-		if rFlag and lFlag:
-			break
-		else:
-			continue
+	time.sleep(2)
+	#rFlag = False
+	#lFlag = False
 
-	rFlag = False
-	lFlag = False
+	# while True:
+	# 	readData = ser.read()
+	# 	if str(readData.decode()) == "r":
+	# 		rFlag = True
+	# 	if str(readData.decode()) == "l":
+	# 		lFlag = True
+	# 	if rFlag and lFlag:
+	# 		break
+	# 	else:
+	# 		continue
+
+	# rFlag = False
+	# lFlag = False
 
 	if len(coords) > 1:
 		coords[i]="0,"+coords[i]
@@ -46,22 +47,13 @@ def sendCoordinates():
 		return
 
 	while True:
-		readData = ser.read()
-		if str(readData.decode()) == "r":
-			rFlag = True
-		if str(readData.decode()) == "l":
-			lFlag = True		
-		if rFlag and lFlag:
-			if i<len(coords)-1:
-				coords[i]="1,"+coords[i]
-				ser.write(coords[i].encode())
-				print(coords[i]+": sent")
-				i+=1
-				rFlag = False
-				lFlag = False
-			else:
-				print("data transmission ended")
-				break
+		time.sleep(2)
+		if i<len(coords)-1:
+			coords[i]="1,"+coords[i]
+			ser.write(coords[i].encode())
+			print(coords[i]+": sent")
+			i+=
 		else:
-			continue
+			print("data transmission ended")
+			break
 	return 1
